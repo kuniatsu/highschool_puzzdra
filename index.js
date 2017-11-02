bb4a8329eb176e6e5dac98076e9cfbedf8d12cda
kuniatus <nightmaredes@hotmail.com>
2017-11-02 19:55:11 +0900

    first

---
+++ index.js
@@ -0,0 +1,30 @@
+var selectDrop = null;
+        var moveDrop = null;
+        var tmp = null;
+        var tmp2 = null;
+        $(function(){
+            $('td').click(function(){
+                console.log("click");
+                if(selectDrop == null){
+                    selectDrop = this;    
+                }
+            });
+
+
+            
+
+            $('td').mouseover(function(e){   
+                // console.log("move"); 
+                if(selectDrop!=null && tmp2 != e){
+                    tmp2 = e;
+                    moveDrop = this;     
+                    tmp = e.currentTarget.innerHTML;
+                //     // console.dir(tmp);
+                    $('#'+moveDrop.id).html(selectDrop.innerHTML); 
+                    $('#'+selectDrop.id).html(tmp); 
+                    selectDrop = this;
+                    
+                }
+            });
+
+        });
